5f837e38f7c1725dd829511fd0dd7d538ed61163
Nello Polesello <polesello@infofactory.it>
2023-02-03 18:24:37 +0100

    settings di produzione

--- src/lisbona/settings/production.py
+++ src/lisbona/settings/production.py
@@ -1,34 +1,19 @@
 from .base import *
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'domain.com', 'www.domain.com']
+ALLOWED_HOSTS = ['voxpop.infofactory.it', 'localhost']
 
 
 # Database Postgresql
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
+        'NAME': 'voxpop',
+        'USER': 'voxpop',
+        'PASSWORD': 'Lì es bona!',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
 
-
-# Security settings
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_BROWSER_XSS_FILTER = True
-
-
-# Email settings
-# EMAIL_HOST = ''
-# EMAIL_USE_TLS = True
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = ''
-# EMAIL_HOST_PASSWORD = ''
-# DEFAULT_FROM_EMAIL = 'NOME <email@email.com>'
